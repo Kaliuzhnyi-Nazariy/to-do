ed7d7f55bb33622d009e9b1ec3804d2fa0803e14
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-22 22:56:33 +0300

    validators for both auth forms

---
+++ src/app/components/form/form/validation/signin.validation.ts
@@ -0,0 +1,16 @@
+import { ISigninForm } from "@/app/components/typesInterfaces";
+import * as yup from "yup";
+
+export const signinValidationSchema = yup.object<ISigninForm>({
+  email: yup
+    .string()
+    .email("Enter valid email value")
+    .min(2, "Must be longer than two characters")
+    .required("Email is required!"),
+  password: yup
+    .string()
+    .min(6, "Must be longer than 6 characters")
+    .max(16, "Must be shorter than 16 characters")
+    .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,16}$/)
+    .required("Password is required!"),
+});
---
+++ src/app/components/form/form/validation/signup.validation.ts
@@ -0,0 +1,25 @@
+import { ISignupForm } from "@/app/components/typesInterfaces";
+import * as yup from "yup";
+
+export const signupValidationSchema = yup.object<ISignupForm>({
+  email: yup
+    .string()
+    .email("Enter valid email value")
+    .required("Email field is required!"),
+  name: yup
+    .string()
+    .min(2, "Must be longer than 2 characters!")
+    .required("Name is required!"),
+  password: yup
+    .string()
+    .min(6, "Must be longer than 6 characters")
+    .max(16, "Must be shorter than 16 characters")
+    .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,16}$/)
+    .required("Password is required!"),
+  confirmPassword: yup
+    .string()
+    .min(6, "Must be longer than 6 characters")
+    .max(16, "Must be shorter than 16 characters")
+    .matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,16}$/)
+    .required("Password is required!"),
+});
