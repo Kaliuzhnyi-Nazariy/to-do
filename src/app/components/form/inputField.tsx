678be7ed05966919f8b576e9c95ba1cb8a0d7634
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-28 22:52:36 +0300

    change placeholder

--- src/app/components/form/inputField.tsx
+++ src/app/components/form/inputField.tsx
@@ -16,7 +16,7 @@ export default function InputField({
       <Field
         name={name}
         type={type}
-        placeholder="Enter your password..."
+        placeholder="Enter data..."
         className="w-[240px] h-10 bg-white rounded-3xl px-5 placeholder:text-black placeholder:opacity-50 focus-visible:outline-2 focus-visible:outline-[var(--darkerpurple)] text-black md:w-[440px] md:h-16 2xl:h-[50px]"
       />
       <ErrorMessage
