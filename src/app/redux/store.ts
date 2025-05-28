626c185d834198d77db3feaefca25a9e6bb02214
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-28 23:00:18 +0300

    create a general state and provide an info

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,16 +1,51 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/slice";
 import { AuthInitialState } from "./auth/typesOrInterfaces";
-
+import { TodoState } from "./todo/typesOrInterfaces";
+import { todoReducer } from "./todo/slice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import { userReducer } from "./user/slice";
+import { UserState } from "./user/typesORInterfaces";
 export interface IStore {
   auth: AuthInitialState;
+  todo: TodoState;
+  user: UserState;
 }
+
+const authPersistedReducer = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+const persistedAuth = persistReducer(authPersistedReducer, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuth,
+    todo: todoReducer,
+    user: userReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistorStore = persistStore(store);
 
 export default store;
 
