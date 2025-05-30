69604c94f7b2a0d3aec6f476593ba68cc81a2ee8
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-30 23:13:43 +0300

    add additional settings and styles for input field

--- src/app/components/form/inputField.tsx
+++ src/app/components/form/inputField.tsx
@@ -5,9 +5,15 @@ import ErrorComp from "./error";
 export default function InputField({
   name,
   type,
+  isDisabled = false,
+  additionalStyles = "",
+  isTextarea = false,
 }: {
   name: string;
   type: string;
+  isDisabled?: boolean;
+  additionalStyles?: string;
+  isTextarea?: boolean;
 }) {
   return (
     <div className="mt-[15px] w-[240px] md:mt-5">
@@ -17,7 +23,11 @@ export default function InputField({
         name={name}
         type={type}
         placeholder="Enter data..."
-        className="w-[240px] h-10 bg-white rounded-3xl px-5 placeholder:text-black placeholder:opacity-50 focus-visible:outline-2 focus-visible:outline-[var(--darkerpurple)] text-black md:w-[440px] md:h-16 2xl:h-[50px]"
+        className={`w-[240px] h-10 bg-white rounded-3xl px-5 placeholder:text-black placeholder:opacity-50 focus-visible:outline-2 focus-visible:outline-[var(--darkerpurple)] text-black md:w-[440px] md:h-16 2xl:h-[50px] ${additionalStyles} ${
+          isTextarea ? "resize-none relative" : ""
+        }`}
+        disabled={isDisabled}
+        as={isTextarea ? "textarea" : "input"}
       />
       <ErrorMessage
         name={name}
