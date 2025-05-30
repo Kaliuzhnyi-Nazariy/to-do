901f6396ff8efcea42b400e653b26f85eeb19e3f
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-30 23:11:56 +0300

    add additional settings and styles for button

--- src/app/components/form/button.tsx
+++ src/app/components/form/button.tsx
@@ -2,18 +2,24 @@ export default function Button({
   type,
   signup,
   children,
+  additionalStyles = "",
+  isDisabled = false,
 }: {
   type?: "button" | "submit" | "reset";
   signup: boolean;
   children: React.ReactNode;
+  additionalStyles?: string;
+  isDisabled?: boolean;
 }) {
   return (
     <button
       type={type ? type : "submit"}
       className={
-        "bg-[var(--darkpurple)] w-[240px] h-9 rounded-[25px] text-[20px] md:w-[220px] md:h-[84px] md:text-[32px] 2xl:h-[65px] " +
-        (signup ? " mt-[25px] md:mt-10 2xl:mt-[30px]" : " mt-10")
+        "bg-[var(--darkpurple)] w-[240px] h-9 rounded-[25px] text-[20px] md:w-[220px] md:text-[32px] 2xl:h-[65px] " +
+        (signup ? " mt-[25px] md:mt-10 2xl:mt-[30px] " : " mt-10 ") +
+        additionalStyles
       }
+      disabled={isDisabled}
     >
       {children}
     </button>
