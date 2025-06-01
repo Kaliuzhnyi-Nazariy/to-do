387c2c431b63914784c292938ca3dfb324a05003
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-06-02 00:03:11 +0300

    update some logic for password input

--- src/app/components/form/inputField.tsx
+++ src/app/components/form/inputField.tsx
@@ -8,20 +8,22 @@ export default function InputField({
   isDisabled = false,
   additionalStyles = "",
   isTextarea = false,
+  changeType,
 }: {
   name: string;
   type: string;
   isDisabled?: boolean;
   additionalStyles?: string;
   isTextarea?: boolean;
+  changeType?: boolean;
 }) {
   return (
-    <div className="mt-[15px] w-[240px] md:mt-5 max-md:min-h-[80px]">
+    <div className="mt-[15px] w-[240px]  max-md:min-h-[80px]">
       <HeaderField>{name}: </HeaderField>
 
       <Field
         name={name}
-        type={type}
+        type={changeType && type === "password" ? "text" : type}
         placeholder="Enter data..."
         className={`w-[240px] h-10 bg-white rounded-3xl px-5 placeholder:text-black placeholder:opacity-50 focus-visible:outline-2 focus-visible:outline-[var(--darkerpurple)] text-black md:w-[440px] md:h-16 2xl:h-[50px] ${additionalStyles} ${
           isTextarea ? "resize-none relative" : ""
